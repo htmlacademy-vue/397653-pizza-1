b94cf2eb7f7168e70f761c7a31b70ee22662f496
Varvara Zhukova <varyaway@mail.ru>
2022-11-17 12:29:03 +0300

    4. Подключение и использование Vuex

---
+++ src/frontend/src/store/index.js
@@ -0,0 +1,4 @@
+﻿import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
---
+++ src/frontend/src/store/modules/index.js
@@ -0,0 +1,11 @@
+﻿import Auth from '@/store/modules/Auth';
+import Builder from '@/store/modules/Builder';
+import Cart from '@/store/modules/Cart';
+import Orders from '@/store/modules/Orders';
+
+export default {
+  Auth,
+  Builder,
+  Cart,
+  Orders
+};
