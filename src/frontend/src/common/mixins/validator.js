cb1863e6b81bb6f346854aeb8c0844c4cd78e8ab
Varvara Zhukova <varyaway@mail.ru>
2023-03-13 19:56:04 +0300

    5. HTTP. Работа с сервером

--- src/frontend/src/common/mixins/validator.js
+++ src/frontend/src/common/mixins/validator.js
@@ -10,7 +10,7 @@ const rules = {
   email: {
     rule: (value) =>
       value ? emailRegex.test(String(value).toLowerCase()) : true,
-    message: "Электроная почта имеет неверный формат",
+    message: "Электронная почта имеет неверный формат",
   },
 };
 
